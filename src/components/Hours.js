83f3966d33168c9ab5bc88cd933fae6a9c886e1c
Sergio Ávila <sergioavila@users.noreply.github.com>
2024-08-27 18:33:18 +0000

    add hour component

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,12 @@
 // Placeholder to host a component
+const Hours = () => {
+    //get hour and minutes
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    return (<div>
+        <h1>Hora actual {hours}:{minutes}</h1>
+    </div>
+    )
+}
+
+export default Hours;
