fc5c97863bd826ba587a69af7a64ea447f3ac6e9
Sergio Ávila <sergioavila@users.noreply.github.com>
2024-08-27 16:16:31 -0400

    Update Hours.js

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,6 +1,7 @@
 const Hours = () => {
     let hours = new Date().getHours();
     let minutes = new Date().getMinutes();
+    //test
     return (<div>
         <h1>Hora actual {hours}:{minutes}</h1>
     </div>
